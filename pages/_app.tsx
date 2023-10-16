e46c9f9cbc2790eab739d0e59109dfb55b75f43e
Leandro <ramosdossantos.leandro@gmail.com>
2023-10-16 06:40:31 -0300

    Setting up mantine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider  } from "@mantine/core";
 import { theme } from "../theme";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <MantineProvider theme={theme}>
+    
+    <MantineProvider defaultColorScheme="dark" theme={theme}>
       <Head>
         <title>Mantine Template</title>
         <meta
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Paper, Text, Loader, Box, Center, MantineProvider } from "@mantine/core";
 
 export default function IndexPage() {
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <>
+    
+      <Paper>
+          <Center>
+            <Text>SIMPLE TEXT TAG</Text>
+          </Center>
+          <Center>
+            <Text>SIMPLE TEXT TAG SIMPLE TEXT TAG SIMPLE TEXT TAG SIMPLE TEXT TAG</Text>
+          </Center>
+          
+      </Paper>
+      <Center>
+        <Button size="xl">Welcome to Mantine!</Button>
+        <Button size="xl">Welcome to Mantine!</Button>
+        <Loader/>
+      </Center>
+      
+    
+    </>
   );
 }
